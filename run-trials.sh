669d536188d87bac655e4842c6dd314a7764d3d0
Matthew Turner <maturner01@gmail.com>
2018-03-28 22:29:03 -0700

    running fig 11b and 12b on cluster; had to return dict not experiment objects for Pool

--- run-trials.sh
+++ run-trials.sh
@@ -4,8 +4,8 @@
 # Eventually could add arguments to this for the number of trials,
 # box width, noise level, etc.
 
-for K in $(seq $1 $2); do
-     qsub -v N_ITER=$N_ITER -v NETWORK="connected caveman" -v OUTPUT_DIR="reproduce_fm2011_3-28" -v K=$K run-experiment.sub
-     qsub -v N_ITER=$N_ITER -v NETWORK="random short-range" -v OUTPUT_DIR="reproduce_fm2011_3-28" -v K=$K run-experiment.sub
-     qsub -v N_ITER=$N_ITER -v NETWORK="random any-range" -v OUTPUT_DIR="reproduce_fm2011_3-28" -v K=$K run-experiment.sub
+N_ITERATIONS = 10000
+
+for N_PER_CAVE in 3 5 10 20 30 40 50; do
+     qsub -v N_PER_CAVE=$N_PER_CAVE -v N_ITERATIONS=$N_ITERATIONS -v OUTPUT_DIR="data/figure11b_test_3-28" run-fig11.sub
 done
--- scripts/runner.py
+++ scripts/runner.py
@@ -1,54 +1,56 @@
 import click
 import multiprocessing as mp
+import networkx as nx
 import numpy as np
 import os
 import pickle
 
 from joblib import Parallel, delayed
 from functools import partial
+from uuid import uuid4
 
 from experiments.within_box import BoxedCavesExperiment
-
+from reproduce_fm2011 import persist_experiments
 
 
 
 def _run_exp(_, experiment='connected caveman', n_caves=20, n_per_cave=5,
-             K=2, n_iter=200, verbose=True):
+             K=2, n_iterations=200, verbose=False):
 
     # Add the same number random short-range or long-range ties.
     n_edges = 20
 
     if experiment == 'connected caveman':
-        cc = BoxedCavesExperiment(5, 20, 1.0, K=K)
-        cc.iterate(n_iter, verbose=verbose)
-        return cc
+        cc = BoxedCavesExperiment(n_caves, n_per_cave, 1.0, K=K)
+        cc.iterate(n_iterations, verbose=verbose)
+        ret = cc
 
     elif experiment == 'random short-range':
         # Connected caveman with short-range ties added randomly.
         ccsrt = BoxedCavesExperiment(n_caves, n_per_cave, 1.0, K=K)
-        # ccsrt.iterate(2000, verbose=False)
-        # ccsrt.add_shortrange_random_edges(n_edges)
-        # ccsrt.iterate(n_iter - 2000, verbose=False)
-        ccsrt.iterate(n_iter - 100, verbose=verbose)
+        ccsrt.iterate(2000, verbose=False)
         ccsrt.add_shortrange_random_edges(n_edges)
-        ccsrt.iterate(100, verbose=verbose)
-        return ccsrt
+        ccsrt.iterate(n_iterations - 2000, verbose=False)
+        ret = ccsrt
 
     elif experiment == 'random any-range':
         # Connected caveman with any-range ties added randomly.
         ccrt = BoxedCavesExperiment(n_caves, n_per_cave, 1.0, K=K)
-        # ccrt.iterate(2000, verbose=False)
-        # ccrt.add_random_edges(n_edges)
-        # ccrt.iterate(n_iter - 2000, verbose=False)
-        ccrt.iterate(n_iter - 100, verbose=verbose)
+        ccrt.iterate(2000, verbose=False)
         ccrt.add_random_edges(n_edges)
-        ccrt.iterate(100, verbose=verbose)
-        return ccrt
+        ccrt.iterate(n_iterations - 2000, verbose=False)
+        ret = ccrt
 
     else:
         raise RuntimeError(
             'experiment type ' + experiment + ' not recognized.'
         )
+
+    return {
+        'polarization': ret.history['polarization'],
+        'final coords': ret.history['final coords'],
+        'graph': nx.to_numpy_matrix(ret.network.graph)
+    }
 
 
 @click.group()
@@ -56,10 +58,55 @@ def _run_exp(_, experiment='connected caveman', n_caves=20, n_per_cave=5,
 def cli(ctx):
     ctx.obj = dict()
 
+@cli.command()
+@click.argument('k', type=int)
+@click.argument('output_dir')
+@click.option('--n_trials', default=5)
+@click.option('--n_iterations', default=10000)
+@click.pass_context
+def reproduce_fig12(ctx, k, output_dir, n_trials, n_iterations):
+    '''
+    Create a set of HDF files corresponding to datasets for reproducing Figure 12b in FM2011
+    '''
+    # Set some processors aside for Numpy computations.
+    pool = mp.Pool(max(2, mp.cpu_count() - 6))
+
+    func = partial(_run_exp, n_iterations=n_iterations,
+                   experiment='connected caveman', K=k)
+    cc_experiments = pool.imap(func, range(n_trials))
+    print('completed connected caveman')
+
+    func = partial(_run_exp, n_iterations=n_iterations,
+                   experiment='random short-range', K=k)
+    srt_experiments = pool.imap(func, range(n_trials))
+    print('completed short-range')
+
+    func = partial(_run_exp, n_iterations=n_iterations,
+                   experiment='random any-range', K=k)
+    any_experiments = pool.imap(func, range(n_trials))
+    print('completed any-range')
+
+    experiments = {
+        'connected caveman': cc_experiments,
+        'random short-range': srt_experiments,
+        'random any-range': any_experiments
+    }
+
+    output_path = os.path.join(output_dir, str(uuid4()) + '.hdf5')
+
+    persist_experiments(
+        experiments,
+        hdf_filename=output_path,
+        metadata={'K': k}
+    )
 
 @cli.command()
+@click.argument('n_per_cave', type=int)
+@click.argument('output_dir')
+@click.option('--n_trials', default=5)
+@click.option('--n_iterations', default=10000)
 @click.pass_context
-def reproduce_fig11(ctx, n_per_cave, save_dir, n_trials=5):
+def reproduce_fig11(ctx, n_per_cave, output_dir, n_trials, n_iterations):
     '''
     Create a set of HDF files corresponding to datasets for reproducing Figure 11b in FM2011
     '''
@@ -67,42 +114,35 @@ def reproduce_fig11(ctx, n_per_cave, save_dir, n_trials=5):
 
     pool = mp.Pool(mp.cpu_count())
 
-    func = partial(_run_exp,
+    func = partial(_run_exp, n_iterations=n_iterations,
                    experiment='connected caveman', n_per_cave=n_per_cave)
     cc_experiments = pool.imap(func, range(n_trials))
     print('completed connected caveman')
 
-    func = partial(_run_exp,
-                   experiment='connected caveman', n_per_cave=n_per_cave)
+    func = partial(_run_exp, n_iterations=n_iterations,
+                   experiment='random short-range', n_per_cave=n_per_cave)
     srt_experiments = pool.imap(func, range(n_trials))
     print('completed short-range')
 
-    func = partial(_run_exp,
-                   experiment='connected caveman', n_per_cave=n_per_cave)
+    func = partial(_run_exp, n_iterations=n_iterations,
+                   experiment='random any-range', n_per_cave=n_per_cave)
     any_experiments = pool.imap(func, range(n_trials))
     print('completed any-range')
 
     experiments = {
-        'connected caveman': list(cc_experiments),
-        'random short-range': list(srt_experiments),
-        'random any-range': list(any_experiments)
+        'connected caveman': cc_experiments,
+        'random short-range': srt_experiments,
+        'random any-range': any_experiments
     }
+
+    output_path = os.path.join(output_dir, str(uuid4()) + '.hdf5')
 
     persist_experiments(
         experiments,
-        directory=save_dir,
-        hdf_filename='multiprocessing_persist.hdf5',
+        hdf_filename=output_path,
         metadata={'K': 2, 'n_per_cave': n_per_cave}
     )
 
-
-@cli.command()
-@click.pass_context
-def reproduce_fig12(ctx, K, n_trials=5):
-    '''
-    Create a set of HDF files corresponding to datasets for reproducing Figure 12b in FM2011
-    '''
-    n_per_cave = 5
 
 # @cli.command()
 # @click.option('--n-trials', default=10,
