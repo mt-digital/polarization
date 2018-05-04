cd24308fff3c5b647f9551ca304f11f18031e98b
Matthew Turner <maturner01@gmail.com>
2018-05-04 04:12:45 -0700

    added vis features

--- vis_graph_connection.py
+++ vis_graph_connection.py
@@ -7,17 +7,28 @@ import seaborn as sns
 from experiments.within_box import BoxedCavesExperiment
 
 
-def vis_graph(n_caves, n_per_cave, l=2, lp=0.2, randomize=False):
+def vis_graph(n_caves, n_per_cave, l=2, lp=0.2, cave_adjust_angle=1.625*np.pi,
+              randomize=False, save_path=None, disconnected=False,
+              figsize=(8, 8)):
 
-    pos = [(
-            (l * np.cos(i * 2 * np.pi / n_caves)) +
-            (lp * np.cos(j * 2 * np.pi / n_per_cave)),
-            (l * np.sin(i * 2 * np.pi / n_caves)) +
-            (lp * np.sin(j * 2 * np.pi / n_per_cave))
-        )
-        for i in range(n_caves)
-        for j in range(n_per_cave)
-    ]
+    # Build node positions list.
+    pos = []
+    for i in range(n_caves):
+        for j in range(n_per_cave):
+            # Rotation angle of cave's central point around origin.
+            theta = i * 2 * np.pi / n_caves
+            # Rotation angle of nodes around central cave point.
+            phi = j * 2 * np.pi / n_per_cave + cave_adjust_angle
+            # Correct phi to keep same side of cave facing center in all caves.
+            psi = phi - ((np.pi/2.0) - theta)
+
+            newpos = (
+                (l * np.cos(theta)) +
+                (lp * np.cos(psi)),
+                (l * np.sin(theta)) +
+                (lp * np.sin(psi))
+            )
+            pos.append(newpos)
 
     cc = BoxedCavesExperiment(n_caves, n_per_cave, 1.0, 2)
     if randomize:
@@ -25,17 +36,27 @@ def vis_graph(n_caves, n_per_cave, l=2, lp=0.2, randomize=False):
 
     gr = cc.network.graph
 
+    if disconnected:
+        gr = nx.caveman_graph(n_caves, n_per_cave)
+
     pos_dict = {
         node: pos[idx] for idx, node in enumerate(gr.nodes())
     }
 
-    # colors = sns.color_palette(sns.diverging_palette(255, 133, l=60,
-    #                      n=n_caves, center="dark"))
-    # colors = sns.color_palette('husl', n_caves)
     colors = sns.color_palette('hls', n_caves)
 
-    node_colors = [colors[gr.node[node]['cave_idx']] for node in pos_dict.keys()]
+    if disconnected:
+        n_agents = n_caves * n_per_cave
+        node_colors = [colors[ii // n_per_cave] for ii in range(n_agents)]
+    else:
+        node_colors = [colors[gr.node[node]['cave_idx']]
+                       for node in pos_dict.keys()]
 
-    plt.figure()
+    plt.figure(figsize=figsize)
 
     nx.draw(gr, pos=pos_dict, node_color=node_colors)
+
+    if save_path is not None:
+        plt.savefig(save_path)
+
+    return cc
